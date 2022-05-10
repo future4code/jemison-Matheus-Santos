21649c65fd04bbaf7f9b0136c3786930e0255d1e
Matheus Queiroz dos Santos <matheusqueirozds@gmail.com>
2022-05-10 20:14:54 -0300

    Atualizei os exercícios de revisão

---
+++ modulo1/revisao-js/exercicios.js
@@ -0,0 +1,79 @@
+// ATENÇÃO!!!
+//    -> NÃO COMENTE NENHUMA DAS FUNÇÕES DECLARADAS!!!
+//    -> NÃO MODIFIQUE OS PARÂMETROS DAS FUNÇÕES!!! ()
+
+// EXERCÍCIO 01
+/* const array = [3, 2, 1, 4, 7];
+
+function retornaTamanhoArray(array) {
+    return array.length;
+} */
+
+// EXERCÍCIO 02
+/* const array = [8, 23, 16, 10];
+
+function retornaArrayInvertido(array) {
+    array.reverse();
+
+    return array;
+} */
+
+// EXERCÍCIO 03
+/* const array = [90, 50, 80, 20, 10];
+
+function retornaArrayOrdenado(array) {
+    array.sort((a, b) => a - b);
+
+    return array;
+} */
+
+// EXERCÍCIO 04
+const array = [1, 2, 3, 4, 5, 6];
+
+function retornaNumerosPares(array) {
+    if (value % 2 === 0) {
+        return value;
+    }
+
+    const numerosPares = array.filter(retornaNumerosPares);
+    console.log(numerosPares);
+}
+
+// EXERCÍCIO 05
+function retornaNumerosParesElevadosADois(array) {}
+
+// EXERCÍCIO 06
+function retornaMaiorNumero(array) {}
+
+// EXERCÍCIO 07
+function retornaObjetoEntreDoisNumeros(num1, num2) {}
+
+// EXERCÍCIO 08
+function retornaNPrimeirosPares(n) {}
+
+// EXERCÍCIO 09
+function classificaTriangulo(ladoA, ladoB, ladoC) {}
+
+// EXERCÍCIO 10
+function retornaSegundoMaiorESegundoMenor(array) {}
+
+// EXERCÍCIO 11
+function retornaChamadaDeFilme(filme) {}
+
+// EXERCÍCIO 12
+function retornaPessoaAnonimizada(pessoa) {}
+
+// EXERCÍCIO 13A
+function retornaPessoasAutorizadas(pessoas) {}
+
+// EXERCÍCIO 13B
+function retornaPessoasNaoAutorizadas(pessoas) {}
+
+// EXERCÍCIO 14
+function retornaContasComSaldoAtualizado(contas) {}
+
+// EXERCÍCIO 15A
+function retornaArrayOrdenadoAlfabeticamente(consultas) {}
+
+// EXERCÍCIO 15B
+function retornaArrayOrdenadoPorData(consultas) {}
