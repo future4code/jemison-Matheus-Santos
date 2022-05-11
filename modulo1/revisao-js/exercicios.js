50550dddc3efaa4e7aa50d082c2c76b437bfdb50
Matheus Queiroz dos Santos <matheusqueirozds@gmail.com>
2022-05-11 13:52:14 -0300

    Atualizei com 10 exercícios realizados

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -60,7 +60,7 @@ function retornaMaiorNumero(array) {
 
 // EXERCÍCIO 07
 
-function retornaObjetoEntreDoisNumeros(num1, num2) {
+/* function retornaObjetoEntreDoisNumeros(num1, num2) {
     if (num1 > num2) {
         maiorNumero = num1;
         menorNumero = num2;
@@ -77,9 +77,10 @@ function retornaObjetoEntreDoisNumeros(num1, num2) {
         maiorDivisivelPorMenor: maiorDivisivelPorMenor,
         diferenca: diferenca,
     });
-}
+} */
 
 // EXERCÍCIO 08
+
 function retornaNPrimeirosPares(n) {}
 
 // EXERCÍCIO 09
@@ -89,13 +90,53 @@ function classificaTriangulo(ladoA, ladoB, ladoC) {}
 function retornaSegundoMaiorESegundoMenor(array) {}
 
 // EXERCÍCIO 11
-function retornaChamadaDeFilme(filme) {}
+/* const filme = {
+    nome: "O Diabo Veste Prada",
+    ano: 2006,
+    diretor: "David Frankel",
+    atores: ["Meryl Streep", "Anne Hathaway", "Emily Blunt", "Stanley Tucci"],
+};
+
+function retornaChamadaDeFilme(filme) {
+    const chamadaDoFilme = `Venha assistir ao filme ${filme.nome}, de ${filme.ano}, dirigido por ${filme.diretor} e estrelado por ${filme.atores[0]}, ${filme.atores[1]}, ${filme.atores[2]}, ${filme.atores[3]}.`;
+
+    return chamadaDoFilme;
+} */
 
 // EXERCÍCIO 12
-function retornaPessoaAnonimizada(pessoa) {}
+/* const pessoa = {
+    nome: "Matheus",
+    idade: 26,
+    email: "matheusqueirozds@gmail.com",
+    endereco: "Salvador, BA",
+};
+
+pessoa.nome = "Anônimo";
+
+function retornaPessoaAnonimizada(pessoa) {
+    const pessoaAnonima = {
+        ...pessoa,
+        nome: "Anônimo".toUpperCase(),
+    };
+    return pessoaAnonima;
+} */
 
 // EXERCÍCIO 13A
-function retornaPessoasAutorizadas(pessoas) {}
+
+const pessoas = [
+    { nome: "Paula", idade: 12, altura: 1.8 },
+    { nome: "João", idade: 20, altura: 1.3 },
+    { nome: "Pedro", idade: 15, altura: 1.9 },
+    { nome: "Luciano", idade: 22, altura: 1.8 },
+    { nome: "Artur", idade: 10, altura: 1.2 },
+    { nome: "Soter", idade: 70, altura: 1.9 },
+];
+
+function retornaPessoasAutorizadas(pessoas) {
+    return pessoas.filter((pessoa) => {
+        return pessoa.altura >= 1.5 && pessoa.idade > 14 && pessoa.idade < 60;
+    });
+}
 
 // EXERCÍCIO 13B
 function retornaPessoasNaoAutorizadas(pessoas) {}
