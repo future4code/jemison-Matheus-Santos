9fa075e01203237ed42277b6969c4e0f230d2b19
Matheus Queiroz dos Santos <matheusqueirozds@gmail.com>
2022-07-05 20:06:07 -0300

    Iniciei o exercício

--- modulo3/estados-insta4/src/components/Post/Post.js
+++ modulo3/estados-insta4/src/components/Post/Post.js
@@ -114,7 +114,7 @@ function Post(props) {
                         valorContador={numeroComentarios}
                     />
                 </PostFooter>
-                {componenteComentario}
+                {componenteComentario} 
             </PostContainer>
 
             <PostContainer>
