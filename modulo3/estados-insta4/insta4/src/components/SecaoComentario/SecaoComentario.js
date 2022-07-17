a7dceaecef6801231fb6e150359934797bbc2437
Matheus Queiroz dos Santos <matheusqueirozds@gmail.com>
2022-07-17 10:23:10 -0300

    Finalizei a atividade do Insta4

---
+++ modulo3/estados-insta4/insta4/src/components/SecaoComentario/SecaoComentario.js
@@ -0,0 +1,25 @@
+import React from "react";
+import styled from "styled-components";
+
+const CommentContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    padding: 5px;
+`;
+
+const InputComentario = styled.input`
+    width: 100%;
+    margin-right: 5px;
+`;
+export function SecaoComentario(props) {
+    return (
+        <CommentContainer>
+            <InputComentario
+                placeholder={"Comentário"}
+                value={""}
+                onChange={props.onChangeComentario}
+            />
+            <button onClick={props.aoEnviar}>Enviar</button>
+        </CommentContainer>
+    );
+}
