e6794f1c91697e99560811412388b4725cdef547
Matheus Queiroz dos Santos <matheusqueirozds@gmail.com>
2022-10-06 20:35:16 -0300

    Atualizei os exercicios

--- modulo5/revisao-javascript-backend/exercicio1/index.js
+++ modulo5/revisao-javascript-backend/exercicio1/index.js
@@ -1,1 +1,33 @@
-const somaDoisNumeros = () => {};
+const convertFahrenheitToCelsius = (fahrenheit) => {
+   const celsius = parseFloat((fahrenheit - 32) / 1.8);
+
+   return `${celsius}°C é equivalente a ${fahrenheit}°F.`;
+};
+console.log(convertFahrenheitToCelsius(50));
+
+/* =============================================== */
+const convertKelvinToCelsius = (Kelvin) => {
+   const celsius = parseFloat(Kelvin - 273.15);
+
+   return `${celsius}°C é equivalente a ${Kelvin}k.`;
+};
+console.log(convertKelvinTocelsius(1200));
+
+/* =============================================== */
+/* const tempConvert = (celsius, escala) => {
+   let fahrenheit = celsius * 1.8 + 32;
+   let kelvin = celsius + 273.15;
+
+   if (isNaN(fahrenheit) || isNaN(celsius) || isNaN(kelvin)) {
+      return console.log(`Erro. Parâmetro inválido (valor)`);
+   } else if (
+      escala == !"F" ||
+      escala == !"f" ||
+      escala == !"K" ||
+      escala == !"k"
+   ) {
+      return console.log(`Erro. Parâmetro inválido (escala)`);
+   }
+};
+
+tempConvert(); */
